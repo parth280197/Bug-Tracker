33337b859b177ed984a5f7cfd3e420bb1898fd3a
Parth <parth280197@gmail.com>
2019-09-20 11:32:30 -0500

    AssignRole method added in UserHelper.cs

--- BugTracker/Helper/UserHelper.cs
+++ BugTracker/Helper/UserHelper.cs
@@ -1,17 +1,20 @@
 ﻿using BugTracker.Models;
 using Microsoft.AspNet.Identity;
 using Microsoft.AspNet.Identity.EntityFramework;
+using System;
 
 namespace BugTracker.Helper
 {
   public class UserHelper
   {
-    ApplicationDbContext _db;
-    RoleManager<IdentityRole> roleManager;
+    private ApplicationDbContext _db;
+    private RoleManager<IdentityRole> _roleManager;
+    private UserManager<IdentityUser> _userManager;
     public UserHelper(ApplicationDbContext db)
     {
       this._db = db;
-      roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_db));
+      _roleManager = new RoleManager<IdentityRole>(new RoleStore<IdentityRole>(_db));
+      _userManager = new UserManager<IdentityUser>(new UserStore<IdentityUser>(_db));
     }
 
     /// <summary>
@@ -20,7 +23,24 @@ namespace BugTracker.Helper
     /// <param name="roleName">Name to be added in roles table.</param>
     public void DefineNewRole(string roleName)
     {
-      roleManager.Create(new IdentityRole(roleName));
+      _roleManager.Create(new IdentityRole(roleName));
+    }
+
+    /// <summary>
+    /// Assign user to the specified role.
+    /// </summary>
+    /// <param name="userId">for User to be assigned to role.</param>
+    /// <param name="roleName">for Role to be associated with user.</param>
+    public void AssignRole(string userId, string roleName)
+    {
+      if (!string.IsNullOrEmpty(userId) && !string.IsNullOrEmpty(roleName))
+      {
+        _userManager.AddToRole(userId, roleName);
+      }
+      else
+      {
+        throw new NullReferenceException("userId and roleName can't be null!");
+      }
     }
   }
 }
