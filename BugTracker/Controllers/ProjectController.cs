e0a8a3c1ef5980169bfd86b89b18c92d9965c313
Parth <parth280197@gmail.com>
2019-09-22 10:26:44 -0500

    CreateOrUpdateform added for the Projects

--- BugTracker/Controllers/ProjectController.cs
+++ BugTracker/Controllers/ProjectController.cs
@@ -1,17 +1,36 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using BugTracker.Models;
+using BugTracker.Models.ViewModel;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 
 namespace BugTracker.Controllers
 {
-    public class ProjectController : Controller
+  public class ProjectController : Controller
+  {
+    ApplicationDbContext db;
+    public ProjectController()
     {
-        // GET: Project
-        public ActionResult Index()
-        {
-            return View();
-        }
+      db = new ApplicationDbContext();
     }
+    // GET: Project
+    public ActionResult Index()
+    {
+      return View();
+    }
+    public ActionResult CreateOrUpdateForm()
+    {
+      ProjectFormViewModel viewModel = new ProjectFormViewModel()
+      {
+        Project = new Project(),
+        UsersList = new SelectList(db.Users.ToList(), "Id", "UserName")
+      };
+      return View(viewModel);
+    }
+
+    [HttpPost]
+    public ActionResult CreateOrUpdateForm(ProjectFormViewModel viewModel)
+    {
+      return View(viewModel);
+    }
+  }
 }
---
+++ BugTracker/Models/ViewModel/ProjectFormViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
+
+namespace BugTracker.Models.ViewModel
+{
+  public class ProjectFormViewModel
+  {
+    public Project Project { get; set; }
+    [Display(Name = "Select users for the project.")]
+    public IEnumerable<SelectListItem> UsersList { get; set; }
+
+    [Required(ErrorMessage = "*Please select user.")]
+    public string[] SelectedId { get; set; }
+  }
+}
