a566f5392531334c3370f50b223fff5ce4de02b4
Parth <parth280197@gmail.com>
2019-09-23 13:36:57 -0500

    PriorityType controller and Views added

---
+++ BugTracker/Controllers/TicketController.cs
@@ -0,0 +1,18 @@
+﻿using System.Web.Mvc;
+
+namespace BugTracker.Controllers
+{
+  public class TicketController : Controller
+  {
+    // GET: Tickets
+    public ActionResult Index()
+    {
+      return View();
+    }
+    public ActionResult Create()
+    {
+      return View();
+    }
+
+  }
+}
